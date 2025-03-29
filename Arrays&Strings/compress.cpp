8a2051f130cdacf0b0ca7f06559f9599d6800e8b
Aniket Yadav <yadavaniket7611@gmail.com>
2025-03-29 15:19:37 +0530

    compresse

---
+++ Arrays&Strings/compress.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    public:
+        int compress(vector<char>& chars) {
+            int n= chars.size();
+            if(n==1){
+                return 1;
+        }
+            int i =0, j=0;
+            while(i<n){
+                char curr = chars[i];
+                int cnt =0;
+                while(i<n && chars[i] == curr){
+                    i++;
+                    cnt++;
+                }
+                chars[j++] = curr;
+                if(cnt>1){
+                    string s = to_string(cnt);
+                    for(char c : s){
+                        chars[j++] = c;
+                    }
+                }
+            }
+            
+            return j;
+        }
+    };
